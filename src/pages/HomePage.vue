adade8f17593e12984186cff5a564e207fcc5b79
GLCVLL <g.ioele96@outlook.it>
2024-02-01 17:12:26 +0100

    created Home page example

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            services: [],
+            isLoading: false
+        }
+    },
+    methods: {
+        async getUsers() {
+            const apiUrl = import.meta.env.VITE_BASEURI;
+            try {
+                this.isLoading = true;
+                const { data } = await this.$axios.get(apiUrl + '/api/services');
+                this.services = data.services;
+            } catch (e) {
+                console.error(e);
+            } finally {
+                this.isLoading = false;
+            }
+
+        }
+    },
+    mounted() {
+        this.getUsers();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Home Page</h1>
+        <section>
+            <h2>Utenti</h2>
+            <div v-if="isLoading" class="h3">Loading...</div>
+            <ul v-else>
+                <li v-for="service in services" :key="service.id">
+                    <h5>{{ service.name }}</h5>
+                </li>
+            </ul>
+        </section>
+    </div>
+</template>
+
+<style></style>
