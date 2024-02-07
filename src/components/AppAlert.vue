6d5cacf54f1150db11ebc01487e4e299e23d6fcd
GLCVLL <g.ioele96@outlook.it>
2024-02-07 17:47:28 +0100

    created appAlert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'success',
+        },
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="alert fade show" :class="`alert-${type}`" role="alert">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped></style>
