621b59a0c36c5f92498fbb1a19f8dbbfcc5ffc68
GLCVLL <g.ioele96@outlook.it>
2024-02-08 11:43:00 +0100

    added register route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // pages
 import HomePage from '../pages/HomePage.vue';
+import RegisterPage from '../pages/RegisterPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
 import AppoitmentPage from '../pages/AppointmentPage.vue';
 
@@ -12,6 +13,7 @@ import auth from '../routes/middlewares/auth';
 // Routes
 const routes = [
     { path: '/', component: HomePage, name: 'home', beforeEnter: auth.user },
+    { path: '/register', component: RegisterPage, name: 'register', beforeEnter: auth.guest },
     { path: '/login', component: LoginPage, name: 'login', beforeEnter: auth.guest },
     { path: '/appointments', component: AppoitmentPage, name: 'appointments', beforeEnter: auth.user},
 
