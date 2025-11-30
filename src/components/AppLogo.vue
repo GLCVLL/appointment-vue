a8fec9fe525bb1e89b983df723a5ff888fbe393e
vmazzarella <vmazzarella@extelos.com>
2025-11-30 17:49:15 +0100

    feat: add AppLogo component to display application name

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// DATA
+const appName = import.meta.env.VITE_APP_NAME;
+</script>
+
+<template>
+  <div class="font-bold text-2xl">
+    {{ appName }}
+  </div>
+</template>
