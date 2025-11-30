c5aa72c4678c273902b27a9433fc12240e4868cc
vmazzarella <vmazzarella@extelos.com>
2025-11-30 17:49:32 +0100

    feat: add VITE_APP_NAME to environment configuration for application identification

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,14 @@
 import { AxiosInstance } from 'axios';
 import { Router } from 'vue-router';
 
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $axios: AxiosInstance;
