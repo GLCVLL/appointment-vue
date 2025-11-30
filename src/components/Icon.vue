481c3e53f80b376f7e73369bc0fe900d16fd130c
vmazzarella <vmazzarella@extelos.com>
2025-11-30 18:01:22 +0100

    feat: add Icon component for dynamic icon rendering using Material Symbols

---
+++ src/components/Icon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed, PropType } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String as PropType<IconName>,
+    required: true,
+  },
+  size: {
+    type: String,
+    default: "24px",
+  },
+});
+
+// ICONS MAP
+const iconsMap = {
+  user: "person",
+} as const;
+type IconName = keyof typeof iconsMap;
+
+const iconSize = computed(() => {
+  if (typeof props.size === "number") {
+    return `${props.size}px`;
+  }
+  return props.size;
+});
+</script>
+
+<template>
+  <span class="material-symbols-outlined" :style="{ fontSize: iconSize }">
+    {{ iconsMap[props.name] }}
+  </span>
+</template>
+
+<style scoped>
+.material-symbols-outlined {
+  font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
+}
+</style>
