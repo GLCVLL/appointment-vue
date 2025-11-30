345278bf9c2872b7038807c4d9e1f9821d134b5e
vmazzarella <vmazzarella@extelos.com>
2025-11-30 18:24:29 +0100

    feat: create Card component for reusable card layout in login page

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PrimeCard from "primevue/card";
+
+// PROPS
+const props = defineProps({
+  title: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <PrimeCard class="bg-primary-200 text-primary-800">
+    <template v-if="title" #title>
+      {{ title }}
+    </template>
+    <template #content>
+      <slot />
+    </template>
+  </PrimeCard>
+</template>
--- src/pages/login/_page.vue
+++ src/pages/login/_page.vue
@@ -4,6 +4,7 @@ import { useRouter } from "vue-router";
 import { setUser, User } from "@/store/auth";
 import { useAxios } from "@/composables/useAxios";
 import PageLayout from "@/components/PageLayout.vue";
+import Card from "@/components/Card.vue";
 
 interface FormData {
   email: string;
@@ -65,50 +66,50 @@ const login = async (): Promise<void> => {
 
 <template>
   <PageLayout>
-    <div class="container">
-      <h1>Login Page</h1>
-
-      <div v-if="errors.generic" class="alert alert-danger">
-        {{ errors.generic }}
+    <div class="flex justify-center items-center flex-1">
+      <div class="w-full max-w-md">
+        <Card title="Login">
+          <div v-if="errors.generic" class="alert alert-danger">
+            {{ errors.generic }}
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              :class="{ 'is-invalid': errors.email }"
+              id="email"
+              v-model.trim="form.email"
+            />
+            <div v-if="errors.email" class="invalid-feedback">
+              {{ errors.email }}
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              :class="{ 'is-invalid': errors.password }"
+              id="password"
+              v-model.trim="form.password"
+            />
+            <div v-if="errors.password" class="invalid-feedback">
+              {{ errors.password }}
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary" @click="submitForm">
+            Login
+          </button>
+          <!-- Register Page -->
+          <div class="mt-3">
+            <span>Not registered yet?</span>
+            <router-link to="/register" class="btn btn-secondary"
+              >Register Now</router-link
+            >
+          </div>
+        </Card>
       </div>
-      <section>
-        <div class="mb-3">
-          <label for="email" class="form-label">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            :class="{ 'is-invalid': errors.email }"
-            id="email"
-            v-model.trim="form.email"
-          />
-          <div v-if="errors.email" class="invalid-feedback">
-            {{ errors.email }}
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            :class="{ 'is-invalid': errors.password }"
-            id="password"
-            v-model.trim="form.password"
-          />
-          <div v-if="errors.password" class="invalid-feedback">
-            {{ errors.password }}
-          </div>
-        </div>
-        <button type="button" class="btn btn-primary" @click="submitForm">
-          Login
-        </button>
-        <!-- Register Page -->
-        <div class="mt-3">
-          <span>Not registered yet?</span>
-          <router-link to="/register" class="btn btn-secondary"
-            >Register Now</router-link
-          >
-        </div>
-      </section>
     </div>
   </PageLayout>
 </template>
