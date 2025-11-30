ed945da1a43e4e22dc612c1d25bddb68d397fa5f
vmazzarella <vmazzarella@extelos.com>
2025-11-30 19:27:24 +0100

    feat: update AppLogo component to use RouterLink for navigation to home

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
+
 // DATA
 const appName = import.meta.env.VITE_APP_NAME;
 </script>
 
 <template>
-  <div class="font-bold text-2xl">
+  <RouterLink to="/" class="font-bold text-2xl no-underline">
     {{ appName }}
-  </div>
+  </RouterLink>
 </template>
