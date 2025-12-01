4c8bd7a39999afab93106345ef7b421e7ac692ae
vmazzarella <vmazzarella@extelos.com>
2025-12-01 18:40:04 +0100

    style: update PageLayout component with background color and text color adjustments

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -3,9 +3,9 @@ import AppBar from "@/components/AppBar.vue";
 </script>
 
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col h-screen bg-primary-50 text-primary-800">
     <AppBar />
-    <main class="flex-1 overflow-y-auto p-4">
+    <main class="flex-1 overflow-y-auto p-4 flex flex-col">
       <slot />
     </main>
   </div>
