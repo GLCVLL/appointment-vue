3a104f3fb4dd8070f4bbdb24a882b4054d8ac8cc
vmazzarella <vmazzarella@extelos.com>
2025-12-08 15:19:16 +0100

    style: simplify main layout by removing padding from main element

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -12,7 +12,7 @@ const handleMenuToggle = () => {
   <div class="flex flex-col h-screen bg-primary-50 text-primary-800">
     <AppBar @menuToggle="handleMenuToggle" />
     <div class="flex-1 overflow-y-auto flex flex-col">
-      <main class="p-4 grow">
+      <main class="grow">
         <slot />
       </main>
 
